669b065dfe8f5458277756817d62fac4bab9c151
Sergey Pustovalov <sergey@pustovalov.me>
2026-01-24 18:29:16 +0100

    Initial Commit

---
+++ HockeyTimer/HockeyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyTimerApp.swift
+//  HockeyTimer
+//
+//  Created by Sergey Pustovalov on 24/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HockeyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
